602faf9bf5ba255251823d2785f0f32b41b214d2
Robby Rish <robbyrish13@gmail.com>
2022-02-27 09:31:33 -0500

    started inquirer function

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 const inquirer = require('inquirer');
+
+const userQuery = () => {
+    return inquirer.prompt([
+      {
+        type: 'list',
+        name: 'opening question',
+        message: 'What would you like to do?',
+        choices: ["view all departments", 
+                  "view all roles", 
+                  "view all employees", 
+                  "add a department", 
+                  "add a role", 
+                  "add an employee", 
+                  "update an employee role"]
+      },
+    ]);
+  };
+
+userQuery();
+
+  // 1. Figure out package.json file
+  // 2. Figure out inquirer.js prompts
+  // 3. Figure out how to put user inputs into db
+  // 4. Figure out how to display them with console.table
+  // 5. Figure out how to take screen capture
+  // 6. Figure out how to input video into README
